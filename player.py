e44b26f0bc26577bd78349d838dab019f47a3386
LK <lenny.khazan@gmail.com>
2016-06-22 23:18:52 -0400

    Basic player implementation

---
+++ player.py
@@ -0,0 +1,15 @@
+class Player(object):
+    def __init__(self):
+        self.money = 1500
+        self.jailTimeRemaining = 0
+        self.properties = []
+
+    def pay(self, money):
+        if self.money >= money:
+            self.money -= money
+            return True
+        else:
+            return False
+
+    def receive(self, money):
+        self.money += money
