84c6d0d440e3bba97d6362029787080c0aef0133
michaeljfriedman <mfred240@gmail.com>
2016-08-07 00:07:14 -0400

    Fixed syntax errors in GameState
    At one point we had inconsistencies in our indentation policy
    (spaces vs tabs). As a result, GameState would not run because
    some indentations were tabbed while others were spaced. This was
    (painstakingly) resolved to all tabs.
    
    - Resolved indentations to all tabs

--- gamestate.py
+++ gamestate.py
@@ -15,34 +15,35 @@ In-line:
 '''
 
 class GameState(object):
-  # Constants
-  NUM_HOUSES = 32
-  NUM_HOTELS = 12
+	# Constants
+	NUM_HOUSES = 32
+	NUM_HOTELS = 12
+	NUM_SQUARES = 40
 
-  # Initialization
-  def initialize_players(num_players):
-    players = []
-    for i in range(0, num_players):
-      players.append(Player())
-    return players
+	# Initialization
+	def initialize_players(num_players):
+		players = []
+		for i in range(0, num_players):
+			players.append(Player())
+		return players
 
-  def initialize_squares():
-    squares_file = "squares.txt"
-    squares = []
-    f = open(squares_file, "r")
-    square_names = f.read().split("\n")
-    for square_name in square_names:
-      squares.append(Square(square_name)) # TODO: Initialize squares 1
-    return squares
+	def initialize_squares():
+		squares_file = "squares.txt"
+		squares = []
+		f = open(squares_file, "r")
+		square_names = f.read().split("\n")
+		for square_name in square_names:
+			squares.append(Square(square_name)) # TODO: Initialize squares 1
+		return squares
 
-  def __init__(self, num_players):
+	def __init__(self, num_players):
 		self._players                = initialize_players(num_players)
 		self._squares                = initialize_squares()
 		self._total_houses           = NUM_HOUSES
 		self._total_hotels           = NUM_HOTELS
 
-  # Private
-  def _copy(self):
+	# Private
+	def _copy(self):
 		num_players = len(self._players)
 		copy = GameState(num_players)
 		for i in range(0, num_players):
@@ -53,22 +54,21 @@ class GameState(object):
 		copy._total_hotels = self._total_hotels
 		return copy
 
-  # Getters
-  @property
-  def squares(self):
-  	return self._squares
+	# Getters
+	@property
+	def squares(self):
+		return self._squares
   
-  @property
-  def players(self):
-  	return self._players
+	@property
+	def players(self):
+		return self._players
   
 	def get_owner(self, prop):
 		for player in self._players:
-	  	props = player.props
-	  	for p in props:
-	  		if (p == prop):
-	  			return player
-	  return None
+			for p in player.props:
+				if (p == prop):
+					return player
+		return None
 
 	# Other
 	def are_enough_houses(self, qty):
@@ -76,32 +76,33 @@ class GameState(object):
 
 	def builds_evenly(self, changes):
 		# TODO: 2 - Implement builds_evenly()
+		return None
 
-	def apply(self, changes): 
-    for player, change_in_cash in changes.change_in_cash.iteritems():
-      player.cash += change_in_cash
+	def apply(self, changes):
+		for player, change_in_cash in changes.change_in_cash.iteritems():
+			player.cash += change_in_cash
 
-    for player, new_position in changes.new_position.iteritems():
-      player.position = new_position
+		for player, new_position in changes.new_position.iteritems():
+			player.position = new_position
 
-    for player, added_props in changes.added_props.iteritems():
-      player.props += added_props
+		for player, added_props in changes.added_props.iteritems():
+			player.props += added_props
 
-    for player, removed_props in changes.removed_props.iteritems():
-      for prop in removed_props:
-        player.props.remove(prop)
+		for player, removed_props in changes.removed_props.iteritems():
+			for prop in removed_props:
+				player.props.remove(prop)
 
-    for player, change_in_jail_moves in changes.change_in_jail_moves.iteritems():
-      player.jail_moves += change_in_jail_moves
+		for player, change_in_jail_moves in changes.change_in_jail_moves.iteritems():
+			player.jail_moves += change_in_jail_moves
 
-    for player, change_in_jail_free_count in changes.change_in_jail_free_count.iteritems():
-      player.jail_free_count += change_in_jail_free_count
+		for player, change_in_jail_free_count in changes.change_in_jail_free_count.iteritems():
+			player.jail_free_count += change_in_jail_free_count
 
-    for player, is_in_game in changes.is_in_game.iteritems():
-      player.is_in_game = is_in_game
+		for player, is_in_game in changes.is_in_game.iteritems():
+			player.is_in_game = is_in_game
 
-    for prop, change_in_houses in changes.change_in_houses.iteritems():
-      prop.num_houses += change_in_houses
+		for prop, change_in_houses in changes.change_in_houses.iteritems():
+			prop.num_houses += change_in_houses
 
-    for prop, is_mortgaged in changes.is_mortgaged.iteritems():
-      prop.is_mortgaged = is_mortgaged     
+		for prop, is_mortgaged in changes.is_mortgaged.iteritems():
+			prop.is_mortgaged = is_mortgaged     
