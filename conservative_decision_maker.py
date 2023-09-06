297dd18ffb803625995f9f106a92b5d506bed0aa
Michael J Friedman <michaeljfriedman3@gmail.com>
2023-09-05 23:08:08 -0400

    Refactor building best house to DecisionMaker

--- conservative_decision_maker.py
+++ conservative_decision_maker.py
@@ -6,45 +6,13 @@ from notification_changes import NotificationChanges
 from constants import *
 
 class ConservativeDecisionMaker(DecisionMaker):
-  # Order in which player will build on properties
-  property_ranking = [
-    BOARDWALK,
-    PARK_PLACE,
-
-    NEW_YORK_AVENUE,
-    ST_JAMES_PLACE,
-    TENNESSEE_AVENUE,
-
-    INDIANA_AVENUE,
-    KENTUCKY_AVENUE,
-    ILLINOIS_AVENUE,
-
-    ATLANTIC_AVENUE,
-    VENTNOR_AVENUE,
-    MARVIN_GARDENS,
-
-    VIRGINIA_AVENUE,
-    ST_CHARLES_PLACE,
-    STATES_AVENUE,
-
-    CONNECTICUT_AVENUE,
-    ORIENTAL_AVENUE,
-    VERMONT_AVENUE,
-
-    BALTIC_AVENUE,
-    MEDITERRANEAN_AVENUE,
-
-    PENNSYLVANIA_AVENUE,
-    PACIFIC_AVENUE,
-    NORTH_CAROLINA_AVENUE,
-  ]
-
   # cash_reserve_multiple is the multiple of the most expensive
   # rent on the board
-  def __init__(self, player, cash_reserve_multiple=1, property_ranking=property_ranking):
+  def __init__(self, player, cash_reserve_multiple=1, property_ranking=None):
     super().__init__(player)
     self._cash_reserve_multiple = cash_reserve_multiple
-    self._property_ranking = property_ranking
+    if property_ranking == None:
+      self._property_ranking = DecisionMaker.property_ranking
 
   # Player doesnt bid
   def bid_house_builds(self, player, highest_bid, props_to_build_on, state):
@@ -66,29 +34,22 @@ class ConservativeDecisionMaker(DecisionMaker):
   def will_trade(self, player, proposal, state):
     return False
 
-  # Player will build houses only if they have enough cash
-  # reserve to pay the highest rent times a multiple, and they
-  # build on the best properties first.
+  # Player will build on the best properties that they can afford
+  def build_allocated_houses(self, house_allocation, state):
+    changes = []
+    num_houses = house_allocation[self._player]
+    return None
+
+  # Player will build on the best houses, maintaining enough cash
+  # reserve to pay the highest rent times a multiple
   def respond_to_state(self, state):
     changes = None
     cash_delta = 0
     while self._player.cash + cash_delta >= self._cash_reserve_multiple * self._max_rent(state):
-      can_build = False
-      # print('new_cash={cash}, max_rent={max_rent}'.format(cash=self._player.cash+cash_delta, max_rent=self._cash_reserve_multiple * self._max_rent(state)))
-      for prop_name in self._property_ranking:
-        prop_idx = INDEX[prop_name]
-        prop = state.squares[prop_idx]
-        # print('prop={prop}'.format(prop=str(prop)))
-        # print('can_build={can_build}'.format(can_build=self._can_build(prop, state)))
-        if state.can_build(self._player, prop, pending_changes=changes):
-          change = GameStateChange.build(prop, state)
-          changes = GroupOfChanges.combine([changes, GroupOfChanges(changes=[change])])
-          can_build = True
-          break
-
-      cash_delta = changes.net_change_in_cash(self._player) if changes != None else 0
-      if not can_build:
+      change = self._build_best_house(state, property_ranking=self._property_ranking, pending_changes=changes)
+      if change == None:
         break
-
+      changes = GroupOfChanges.combine([changes, GroupOfChanges(changes=[change])])
+      cash_delta = changes.net_change_in_cash(self._player) if changes != None else 0
     building_requests = BuildingRequests(house_builds=changes)
     return NotificationChanges(building_requests=building_requests)
